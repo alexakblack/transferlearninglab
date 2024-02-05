cc5780176783319b4b3ba84c4770d0b9c832bb03
alexakblack <118311797+alexakblack@users.noreply.github.com>
2024-02-05 11:07:13 -0800

    Add files via upload
    first upload

--- training.py
+++ training.py
@@ -6,6 +6,7 @@ import tensorflow.keras.layers as layers
 import tensorflow.keras.optimizers as optimizers
 import tensorflow.keras.losses as losses
 import argparse
+import tensorflow.saved_model as saved_model
 
 ap = argparse.ArgumentParser()
 
@@ -39,21 +40,7 @@ ap.add_argument(
     help="checkpoint to load model weights from",
     default=None,
 )
-ap.add_argument(
-    "-c",
-    "--checkpoint-save-path",
-    required=False,
-    help="location to save model checkpoint to after every epoch",
-    default=None,
-)
-ap.add_argument(
-    "-f",
-    "--frequency-checkpoint",
-    required=False,
-    help="frequency of saving model checkpoint (in epochs)",
-    default=1,
-    type=int
-)
+
 ap.add_argument(
     "-m",
     "--model",
@@ -80,13 +67,6 @@ ap.add_argument(
 args = vars(ap.parse_args())
 print(f"\n{args=}\n")
 
-# print(f"{mobilenet=}")
-# print(f"{inputs=}")
-# print(f"{outputs=}")
-# print(f"{optimizer=}")
-# print(f"{loss=}")
-# print(f"{model=}")
-
 print("\nTraining model...")
 
 net = resnet50.ResNet50(
@@ -102,19 +82,20 @@ inputs = keras.Input(shape = (224, 224, 3))
 outputs = net(inputs)
 outputs = layers.Dense(18, activation = 'softmax')(outputs)
 
-optimizer = optimizers.legacy.Adam(learning_rate = 0.00001)
+optimizer = optimizers.legacy.Adam(learning_rate = 0.001)
 loss = losses.CategoricalCrossentropy()
 
 model = keras.Model(inputs, outputs)
-
-# if args["load_checkpoint_path"] is not None:
-#     model.load_weights(args["load_checkpoint_path"])
 
 model.compile(
     optimizer = optimizer,
     loss = loss,
     metrics = ['accuracy']
 )
+
+if args['load_checkpoint_path'] != None:
+    model = tf.keras.models.load_model(args['load_checkpoint_path'])
+
 
 train, validation = utils.image_dataset_from_directory(
     'trainingdata',
@@ -125,45 +106,36 @@ train, validation = utils.image_dataset_from_directory(
     subset = 'both',
 )
 
-
 train = train.map(lambda x, y: (resnet50.preprocess_input(x), y))
 validation = validation.map(lambda x, y: (resnet50.preprocess_input(x), y))
 
-model.fit(
-    train,
-    batch_size = 32,
-    epochs = 100,
-    verbose = 1,
-    validation_data = validation,
-    validation_batch_size = 32
-)
 
-if args["checkpoint_save_path"] is not None:
-    if args["frequency_checkpoint"] == 1:
-        save_freq = 'epoch'
-    else:
-        num_batches = len(train)
-        save_freq = args["frequency_checkpoint"] * num_batches
+# if args["checkpoint_save_path"] is not None:
+#     if args["frequency_checkpoint"] == 1:
+#         save_freq = 'epoch'
+#     else:
+#         num_batches = len(train)
+#         save_freq = args["frequency_checkpoint"] * num_batches
 
-    save_callback = keras.callbacks.ModelCheckpoint(
-        filepath = args["checkpoint_save_path"],
-        monitor = "val_accuracy",
-        verbose = 1,
-        save_weights_only = True,
-        save_freq = save_freq,
-    )
-    callbacks = [save_callback]
-else:
-    callbacks = []
+#     save_callback = keras.callbacks.ModelCheckpoint(
+#         filepath = args["checkpoint_save_path"],
+#         monitor = "val_accuracy",
+#         verbose = 1,
+#         save_weights_only = True,
+#         save_freq = save_freq,
+#     )
+#     callbacks = [save_callback]
+# else:
+#     callbacks = []
 
 history = model.fit(
     train,
     batch_size = args["batch_size"],
     epochs = args["epochs"],
     verbose = 1,
-	callbacks = callbacks,
+	#callbacks = callbacks,
     validation_data = validation,
-    validation_batch_size = args["batch_size"]
+    validation_batch_size = args["batch_size"],
 )
 
 print(f"Training finished.")
@@ -196,6 +168,5 @@ if args["plot_history_save_path"]:
 
     with open(args["plot_history_save_path"], "w") as f:
         json.dump(old_history, f, indent=4)
-# ---------------------------------------------------------------------------- #
-# ---------------------------------------------------------------------------- #
+
 
